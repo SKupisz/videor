1619868141b130a4f0c05ed12e5d55a5c111c3a8
SKupisz <szym-ku@wp.pl>
2019-06-05 22:07:18 +0200

    Better side logic, delete from list avaible

--- src/main.js
+++ src/main.js
@@ -2,23 +2,15 @@ if(!window.localStorage.list)
 {
   window.localStorage.list = "";
 }
-
 let content = document.querySelector(".video-shower");
 let inputValue = document.querySelector(".select-input").value;
 
-function playFromList(event){
-  let songName = event.target.childNodes[0].innerHTML;
-  songName.trim();
+function playFromList(songName){
   let localStore = window.localStorage.list;
-  //alert(localStore+" "+songName+"   "+localStore.indexOf(songName,0));
   localStore = localStore.substr(0,localStore.length-1);
   let newLocalStore = localStore.split(";");
   let final = "";
   let address = newLocalStore.indexOf(songName)+1;
-  //alert("/"+newLocalStore[0]+"/  "+newLocalStore.indexOf(songName));
-  /*alert(window.localStorage.list);
-  alert(localStorage.length+" "+address);*/
-  //alert(address);
   if(address <= 0)
   {
     address = localStore.indexOf(songName)+1;
@@ -49,6 +41,37 @@ function playFromList(event){
 
 }
 
+function delFromList(name){
+  let forNow = window.localStorage.list, toGo = "";
+  toGo = forNow.substring(forNow.indexOf(name),forNow.length);
+  let posStart = name.length+1;
+  while(toGo.charAt(posStart) != ';')
+  {
+    posStart++;
+  }
+  toGo = forNow.substring(0,forNow.indexOf(name))+toGo.substr(posStart+1);
+  window.localStorage.list = toGo;
+  toGo.substr(0,toGo.length-1);
+  toGo = toGo.split(";");
+  let final = "";
+  for(var i = 0 ; i < toGo.length-1; i+=2)
+  {
+    final+="<div class = 'fav-row'>"+toGo[i]+"</div><button class = 'delIt'>X</button>";
+    //<button class = 'select-submit row-submit' type = 'submit'>Load video</button>
+  }
+  document.querySelector(".fav-showing").innerHTML = final;
+  let rowList = document.getElementsByClassName("fav-row");
+  let delList = document.getElementsByClassName("delIt");
+  for(let i = 0 ; i < rowList.length; i++)
+  {
+    rowList[i].addEventListener("click",function(event){
+      playFromList(toGo[i*2]);
+    });
+    delList[i].addEventListener("click",function(event){
+      delFromList(toGo[i*2]);
+    });
+  }
+}
 
 document.querySelector(".main-load").addEventListener("click",function(){
   if(document.querySelector(".video-shower").style.display == "none")
@@ -78,7 +101,6 @@ document.querySelector(".main-load").addEventListener("click",function(){
   {
     content.src = inputValue;
   }
-
 });
 
 document.querySelector(".add-submit").addEventListener("click",function(){
@@ -110,7 +132,27 @@ document.querySelector(".add-submit").addEventListener("click",function(){
     }
     window.localStorage.setItem("list", window.localStorage.list+name+";"+inputValue+";");
   }
-
+  let local = window.localStorage.list;
+  local = local.substr(0,local.length-1);
+  local = local.split(";");
+  let final = "";
+  for(var i = 0 ; i < local.length-1; i+=2)
+  {
+    final+="<div class = 'fav-rowContainer'><div class = 'fav-row'>"+local[i]+"</div>"+
+    "<button class = 'delIt'>X</button></div>";
+  }
+  document.querySelector(".fav-showing").innerHTML = final;
+  let rowList = document.getElementsByClassName("fav-row");
+  let delList = document.getElementsByClassName("delIt");
+  for(let i = 0 ; i < rowList.length; i++)
+  {
+    rowList[i].addEventListener("click",function(event){
+      playFromList(local[i*2]);
+    });
+    delList[i].addEventListener("click",function(event){
+      delFromList(local[i*2]);
+    });
+  }
 });
 
 document.querySelector(".fav-submit").addEventListener("click",function(){
@@ -124,9 +166,7 @@ document.querySelector(".fav-submit").addEventListener("click",function(){
     let final = "";
     for(var i = 0 ; i < local.length-1; i+=2)
     {
-      final+="<div class = 'fav-row'><span class = 'fav-name'>"
-      +local[i]+
-      "</span></div>";
+      final+="<div class = 'fav-row'>"+local[i]+"</div><button class = 'delIt'>X</button>";
       //<button class = 'select-submit row-submit' type = 'submit'>Load video</button>
     }
     document.querySelector(".video-shower").style.display = "none";
@@ -134,10 +174,14 @@ document.querySelector(".fav-submit").addEventListener("click",function(){
     document.querySelector(".fav-showing").innerHTML = final;
     document.querySelector(".fav-submit").innerHTML = "Hide favourites list";
     let rowList = document.getElementsByClassName("fav-row");
+    let delList = document.getElementsByClassName("delIt");
     for(let i = 0 ; i < rowList.length; i++)
     {
       rowList[i].addEventListener("click",function(event){
-        playFromList(event);
+        playFromList(local[i*2]);
+      });
+      delList[i].addEventListener("click",function(event){
+        delFromList(local[i*2]);
       });
     }
 
